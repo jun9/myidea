d84318c90a1b01e1ea96409fbc6537726f6b6b42
danjiang <danjiang5956@gmail.com>
2011-12-21 14:29:18 +0800

    add deploy,init data

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,27 @@
+default_run_options[:pty] = true
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, 'ruby-1.9.2-p290@myidea'        # Or whatever env you want it to run in.
+
+set :use_sudo, false
+
 set :application, "myidea"
 
 set :repository,  "git://github.com/danjiang/myidea.git"
 set :scm, :git
 
-set :deploy_to, "/var/www"
+set :deploy_to, "/var/www/myidea"
 
 server "myidea.danthought.com", :app, :web, :db, :primary => true
 
 namespace :deploy do
+  task :copy_database_configuration do 
+    production_db_config = "~/database.yml" 
+    run "cp #{production_db_config} #{release_path}/config/database.yml"
+  end
+  after "deploy:update_code", "deploy:copy_database_configuration"
+
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
---
+++ db/migrate/20111221032413_add_init_data.rb
@@ -0,0 +1,21 @@
+class AddInitData < ActiveRecord::Migration
+  def up
+    User.delete_all
+    User.create(:username => "admin",:email => "admin@example.com",:password =>"123456",:password_confirmation => "123456")
+    user = User.find_by_username('admin')
+    user.admin = true
+    user.save
+    
+    Preference.delete_all
+    Preference.create(:name =>PREFERENCE_SITE_NAME,:value => 'No Site Name')
+
+    Category.delete_all
+    Category.create(:name =>'Default')
+  end
+
+  def down
+    User.delete_all
+    Preference.delete_all
+    Category.delete_all
+  end
+end
